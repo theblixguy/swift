8e79f2cf967a9ecd1182f280d76172fdfdf0f428
Suyash Srijan <suyashsrijan@outlook.com>
2019-08-23 21:38:20 +0100

    [Test] Add a test case

--- test/attr/attr_dynamic_member_lookup.swift
+++ test/attr/attr_dynamic_member_lookup.swift
@@ -722,3 +722,12 @@ struct SR10597_1_W<T> {
 
 _ = SR10597_1_W<SR10597_1>(SR10597_1()).wooo // expected-error {{value of type 'SR10597_1_W<SR10597_1>' has no dynamic member 'wooo' using key path from root type 'SR10597_1'; did you mean 'woo'?}}
 _ = SR10597_1_W<SR10597_1>(SR10597_1()).bla // expected-error {{value of type 'SR10597_1_W<SR10597_1>' has no dynamic member 'bla' using key path from root type 'SR10597_1'}}
+
+// SR-10557
+
+@dynamicMemberLookup // expected-error {{@dynamicMemberLookup attribute requires 'SR_10557_S' to have a 'subscript(dynamicMember:)' method that accepts either 'ExpressibleByStringLiteral' or a keypath}}
+struct SR_10557_S {
+  subscript(dynamicMember: String) -> String { // expected-error {{add an explicit argument label to this subscript to satisfy the @dynamicMemberLookup requirement}}
+    fatalError()
+  }
+}
